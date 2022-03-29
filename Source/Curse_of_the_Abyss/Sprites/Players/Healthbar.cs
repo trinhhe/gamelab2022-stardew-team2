09640edf9d73cf24e021d83f3f8fcf3679162b3d
Yan Walesch <ywalesch@student.ethz.ch>
2022-03-29 17:00:41 +0200

    Healthbar

--- Source/Curse_of_the_Abyss/Sprites/Players/Healthbar.cs
+++ Source/Curse_of_the_Abyss/Sprites/Players/Healthbar.cs
@@ -10,20 +10,22 @@ namespace Curse_of_the_Abyss
     {
         public static Texture2D bar, currhealth_text;
         public int maxhealth, curr_health;
+        public static SpriteFont font;
 
         public Healthbar(int x, int y)
         {
             name = "healthbar";
             position = new Rectangle(x, y, 10, 300);
             collidable = false;
-            maxhealth = 100;
-            curr_health = 100;
+            maxhealth = 5000;
+            curr_health = 5000;
         }
 
         public static void LoadContent(ContentManager content)
         {
             bar = content.Load<Texture2D>("bar");
             currhealth_text = content.Load<Texture2D>("health");
+            font = content.Load<SpriteFont>("O2");
         }
 
         public override void Update()
@@ -33,9 +35,16 @@ namespace Curse_of_the_Abyss
 
         public override void Draw(SpriteBatch spritebatch)
         {
+            //draw bar
             spritebatch.Draw(bar, position, Color.White);
-            Rectangle healthbar = new Rectangle(position.X,position.Y+1,position.Width,position.Height*curr_health/maxhealth-2);
+
+            //draw current health
+            int curr_ypos = position.Y + position.Height - position.Height*curr_health / maxhealth +1;
+            Rectangle healthbar = new Rectangle(position.X,curr_ypos, position.Width,position.Height*curr_health/maxhealth-2);
             spritebatch.Draw(currhealth_text, healthbar, Color.White);
+
+            //draw text
+            spritebatch.DrawString(font,"O2",new Vector2(position.X,position.Bottom+1),Color.Black); 
         }
     }
 }
