cee371b17b827d0e9b21d06e5199a29829305214
Yan Walesch <ywalesch@student.ethz.ch>
2022-03-29 22:39:33 +0200

    Added Player Sprite to game

--- Source/Curse_of_the_Abyss/Sprites/Players/WaterPlayer.cs
+++ Source/Curse_of_the_Abyss/Sprites/Players/WaterPlayer.cs
@@ -25,7 +25,7 @@ namespace Curse_of_the_Abyss
 
         public static void LoadContent(ContentManager content){
             //TO DO: replace SmileyWalk by actual Sprites
-            texture = content.Load<Texture2D>("SmileyWalk");
+            texture = content.Load<Texture2D>("MCRunSprite");
         }
 
         public override void Update(){
@@ -53,8 +53,8 @@ namespace Curse_of_the_Abyss
         public override void Draw(SpriteBatch spritebatch){
             //this block currently chooses one specific frame to draw
             //TO DO: Decide current frame in getState method instead of here
-            int width = texture.Width/4;
-            int height = texture.Width/4;
+            int width = texture.Width/5;
+            int height = texture.Height;
             Rectangle source = new Rectangle(0,0,width,height);
 
             //check if player is doging
