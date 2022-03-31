3e697bad88111ca151edf4451bc72c24dde750e7
Marino Mueller <muemarin@student.ethz.ch>
2022-03-31 18:52:56 +0200

    octopuss

--- Source/Curse_of_the_Abyss/Sprites/NPCs/ShootingSprite.cs
+++ Source/Curse_of_the_Abyss/Sprites/NPCs/ShootingSprite.cs
@@ -31,7 +31,7 @@ namespace Curse_of_the_Abyss
         public static void LoadContent(ContentManager content)
         {
             //TO DO: replace SmileyWalk by actual Sprites
-            texture = content.Load<Texture2D>("SmileyWalk");
+            texture = content.Load<Texture2D>("octopuss");
         }
 
         public override void Update()
@@ -48,8 +48,8 @@ namespace Curse_of_the_Abyss
         {
             //this block currently chooses one specific frame to draw
             //TO DO: Decide current frame in getState method instead of here
-            int width = texture.Width / 4;
-            int height = texture.Width / 4;
+            int width = texture.Width;
+            int height = texture.Width;
             Rectangle source = new Rectangle(0, 0, width, height);
 
 
