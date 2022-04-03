de011f048083befd43bfbf7d881a3bd46c84da8e
Ellen <ellen@null>
2022-04-03 13:01:30 +0200

    fixed eggs

--- Source/Curse_of_the_Abyss/Levels/Level_1.cs
+++ Source/Curse_of_the_Abyss/Levels/Level_1.cs
@@ -22,16 +22,9 @@ namespace Curse_of_the_Abyss
             InitSprites();
             eggs = new EggCollection();
 
-
-             //Add eggs here
-<<<<<<< HEAD
-            
-            eggs.addEgg(1200, 700);
-            eggs.addEgg(1500, 400);
-=======
-            eggs.addEgg(1200, 700);
-            eggs.addEgg(1500, 400);
->>>>>>> 5b006e044a9bf80cf7cc7c23dc3fdd7d40e24d5b
+            //Add eggs here
+            eggs.addEgg(700, 850);
+            eggs.addEgg(800, 800);
         }
 
         //inits every item/character that is not a player or submarine
