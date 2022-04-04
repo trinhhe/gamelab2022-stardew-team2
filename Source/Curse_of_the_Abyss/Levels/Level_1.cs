2217df93d287258575ad92464aa784a5d86f6857
Yan Walesch <ywalesch@student.ethz.ch>
2022-04-04 17:59:44 +0200

    deleted false map

--- Source/Curse_of_the_Abyss/Levels/Level_1.cs
+++ Source/Curse_of_the_Abyss/Levels/Level_1.cs
@@ -34,7 +34,7 @@ namespace Curse_of_the_Abyss
         public Level1()
         {
             // load tile map 
-            TileMap = new TmxMap("./Content/maps/map_lvl1_new.tmx");
+            TileMap = new TmxMap("./Content/maps/map_lvl1.tmx");
             Reset();
         }
 
