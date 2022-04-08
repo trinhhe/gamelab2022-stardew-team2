995850f1dbc363cdac65587139ca086e61a89804
Yan Walesch <ywalesch@student.ethz.ch>
2022-04-08 17:42:36 +0200

    Explosion animation + soundeffect

--- Source/Curse_of_the_Abyss/Sprites/Submarine/Bomb.cs
+++ Source/Curse_of_the_Abyss/Sprites/Submarine/Bomb.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
@@ -8,32 +9,66 @@ namespace Curse_of_the_Abyss
 {
     public class Bomb : MovableSprite
     {
-        public static Texture2D texture;
         public int ground;
         public float linearVelocity;
         private string[] collidables = {"obstacle", "targetingNPC", "rock" };
+        protected AnimationManager animationManager;
+        public static Dictionary<string, Animation> animations;
+        Sprite other;
+        public static SoundEffect explosion;
         public Bomb(int x, int y)
         {
             this.name = "bomb";
             this.position = new Rectangle(x, y, 30, 30);
             this.linearVelocity = Constants.submarine_bomb_velocity;
             collidable = true;
+            animationManager = new AnimationManager(animations["bomb"]);
         }
         public static void LoadContent(ContentManager content)
         {
-            texture = content.Load<Texture2D>("bomb");
+            animations = new Dictionary<string, Animation>()
+            {
+                {"bomb", new Animation(content.Load<Texture2D>("bomb"), 1, 0.5f, false) },
+                {"explosion", new Animation(content.Load<Texture2D>("explosion"), 9, 0.1f, false) } //part of the draw is a stemp from pixilart
+            };
+            explosion = content.Load<SoundEffect>("mixkit-sea-mine-explosion-1184"); //this soundtrack is from mixkit
         }
         public override void Update(List<Sprite> sprites,GameTime gametime)
         {
-            position.Y += (int)linearVelocity;
-            Sprite s = CheckCollision(sprites,collidables);
-            if (s != null) YCollision(s,gametime);
+            if (animationManager.animation == animations["bomb"])
+            {
+                position.Y += (int)linearVelocity;
+                Sprite s = CheckCollision(sprites, collidables);
+                if (s != null) YCollision(s, gametime);
+            }
+            else
+            {
+                animationManager.Update(gametime);
+            }
         }
 
         public override void Draw(SpriteBatch spritebatch)
         {
-            Rectangle pos = new Rectangle(position.X-5, position.Y,position.Width+10,position.Height+10);
-            spritebatch.Draw(texture, pos, null, Color.White, 0, Vector2.Zero, SpriteEffects.None, 0.3f);
+            if (animationManager.animation == animations["bomb"])
+            {
+                Rectangle pos = new Rectangle(position.X - 5, position.Y, position.Width + 10, position.Height + 10);
+                animationManager.Draw(spritebatch, pos,0f);
+            }
+            else
+            {
+                Rectangle pos = new Rectangle(position.X-20, position.Y-20, position.Width + 40, position.Height + 40);
+                animationManager.Draw(spritebatch, pos, 1f);
+                if(animationManager.animation.CurrentFrame == 5)
+                {
+                    animationManager.animation.FrameSpeed = 0.4f;
+                    if (other != null) other.remove = true;
+                }
+                if(animationManager.animation.CurrentFrame == animationManager.animation.FrameCount-1)
+                {
+                    animationManager.animation.FrameSpeed = 0.1f;
+                    remove = true;
+                }
+            }
         }
 
         public override void YCollision(Sprite s, GameTime gametime)
@@ -43,14 +78,26 @@ namespace Curse_of_the_Abyss
                 case ("obstacle"):
                 case ("stationaryNPC"):
                 case ("pathNPC"):
-                    remove = true;
+                    collidable = false;
+                    startExplosion();
+                    break;
+                case ("targetingNPC"):
+                    s.remove = true;
+                    collidable = false;
+                    startExplosion();
                     break;
                 case ("rock"):
-                case ("targetingNPC"):
-                    remove = true;
-                    s.remove = true;
+                    other = s;
+                    collidable = false;
+                    startExplosion();
                     break;
             }
+        }
+
+        private void startExplosion()
+        {
+            animationManager.Play(animations["explosion"]);
+            explosion.Play();
         }
     }
 }
