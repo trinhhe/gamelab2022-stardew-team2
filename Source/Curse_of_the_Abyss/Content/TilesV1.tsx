62902d42745fe868ee5b1714037a03490790bfae
jokics <jokics@student.ethz.ch>
2022-04-20 16:15:24 +0200

    update TilesV1

---
+++ Source/Curse_of_the_Abyss/Content/TilesV1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="TilesV1" tilewidth="32" tileheight="32" tilecount="14" columns="14">
+ <image source="TilesV1.png" width="448" height="32"/>
+</tileset>
