c0d6c69455672e280ff0ae79349b417e032a81e3
jokics <jokics@student.ethz.ch>
2022-05-14 18:49:48 +0200

    Revert "fix prints in bottlnecks.c for alg1"
    This reverts commit de7d75bab59cba48d9d0292cd5803500769a05e1.

--- Source/Curse_of_the_Abyss/Levels/Level_1.cs
+++ Source/Curse_of_the_Abyss/Levels/Level_1.cs
@@ -52,8 +52,8 @@ namespace Curse_of_the_Abyss
             sprites.Add(leftborder);
             SeaUrchin seaUrchin1 = new SeaUrchin(80, 350);
             sprites.Add(seaUrchin1);
-
-            MovingPlatform movableObstacle1 = new MovingPlatform(120, 1022, 120, 540, 2, 128, changedir: true);
+            
+            MovingPlatform movableObstacle1 = new MovingPlatform(120, 1022, 120, 700, 2,128, changedir: true);
             sprites.Add(movableObstacle1);
             MovingPlatform movableObstacle2 = new MovingPlatform(2500, 892, 2500, 350, 2, 50, changedir: true);
             sprites.Add(movableObstacle2);
